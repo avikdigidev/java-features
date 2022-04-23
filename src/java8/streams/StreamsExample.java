8768acfdd488be70df23f526c2604045b95645d3
avikdigidev <avikdigidev@gmail.com>
2022-04-24 01:55:55 +0530

    Predicate filter example using streams

--- src/java8/streams/StreamsExample.java
+++ src/java8/streams/StreamsExample.java
@@ -4,14 +4,18 @@ package java8.streams;
 import java.util.Arrays;
 import java.util.OptionalInt;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class StreamsExample {
     public static void main(String[] args) {
         int[] arr1 = {11, 45, 12, 4, 5, 8, 6, 5, 8, 9, 5, 2, 3, 52, 100, 1, 4, 5};
         Integer[] arr = {11, 45, 12, 4, 5, 8, 6, 5, 8, 9, 5, 2, 3, 52, 100, 1, 4, 5};
 //consumer example
-        Consumer<Integer> c = t-> System.out.println("Print input multiply *3 : " + (t*3));
+        Consumer<Integer> c = t -> System.out.println("Print input multiply *3 : " + (t * 3));
         Arrays.stream(arr).forEach(c);
+        //we can also directly pass like:
+        Arrays.stream(arr).forEach(t -> System.out.println("Print input multiply *4 : " + (t * 4)));
+        Arrays.stream(arr).filter(t-> t>20).forEach(System.out::println);
         int max1 = Arrays.stream(arr1).max().getAsInt();
         System.out.println(max1);
         OptionalInt max2 = Arrays.stream(arr).mapToInt(v -> v)
