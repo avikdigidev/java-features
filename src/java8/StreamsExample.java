857446522bc6d376a58f4cc9de105384df2b853d
avikdigidev <avikdigidev@gmail.com>
2022-04-24 00:15:10 +0530

    max using streams

--- src/java8/StreamsExample.java
+++ src/java8/StreamsExample.java
@@ -1,9 +1,21 @@
 package java8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+
 public class StreamsExample {
     public static void main(String[] args) {
-        int[] arr = {11,,12,4,5,8,6,5,8,9,5,2,3,52,100,1,4,5};
-        
+        int[] arr1 = {11,45,12,4,5,8,6,5,8,9,5,2,3,52,100,1,4,5};
+        Integer[] arr = {11,45,12,4,5,8,6,5,8,9,5,2,3,52,100,1,4,5};
+
+        int max1 =  Arrays.stream(arr1).max().getAsInt();
+        System.out.println(max1);
+        OptionalInt max2 = Arrays.asList(arr).stream().mapToInt(v -> v)
+                .max();
+        System.out.println(max2.getAsInt());
+
     }
 
 }
