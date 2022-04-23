61512c5a84d251d721afe9957613f7502a104c84
avikdigidev <avikdigidev@gmail.com>
2022-04-24 01:45:29 +0530

    Consumer functional interface demo

---
+++ src/java8/fi/ConsumerInterfaceDemo.java
@@ -0,0 +1,10 @@
+package java8.fi;
+
+import java.util.function.Consumer;
+
+public class ConsumerInterfaceDemo {
+    public static void main(String[] args) {
+        Consumer<Integer> c = (t)-> System.out.println("Print input multiply *2 : " + (t*2));
+        c.accept(12);
+    }
+}
